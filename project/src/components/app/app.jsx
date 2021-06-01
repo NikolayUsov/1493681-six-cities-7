8d266c74620e7db789ad40dda7bf7e07849d144f
Nikolay <nikolya_usov@bk.ru>
2021-06-01 15:59:06 +0300

    init: create render main page

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import PageMainComponent from '../page-main/page-main.jsx';
+import PropTypes from 'prop-types';
 
+function App({offers}) {
 
-function App() {
-  return <p>Hello, world!</p>;
+  return(
+    <PageMainComponent offers={offers}/>
+  );
+
 }
+
+App.propTypes  = {
+  offers: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+App.defaultProps = {
+  offers: [],
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const OFFERS_COUNT = 9;
+const createOffers = (offersCount) => {
+  const offers = new Array(offersCount).fill('');
+  return offers.map((elem,index) => {
+    elem = {id: index};
+    return elem;
+  });
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  offers={createOffers(OFFERS_COUNT)}/>
   </React.StrictMode>,
   document.getElementById('root'));
