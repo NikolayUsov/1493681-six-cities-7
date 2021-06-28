f80a057a05b0054fb69d012a1d03774e4c361105
Usov Nikolay <nikolya_usov@bk.ru>
2021-06-28 22:07:49 +0300

    ref: formValidation

--- project/src/pages/login.jsx
+++ project/src/pages/login.jsx
@@ -1,31 +1,19 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import Logo from '../components/logo/logo';
+import Header from '../components/header/header';
 
 // eslint-disable-next-line import/no-named-as-default
-import UserNavigation from '../components/user-navigation/user-navigation';
+
 // eslint-disable-next-line import/no-named-as-default
 import LoginForm from '../components/login-form/login-form';
 
 export default function Login() {
   return (
     <div className="page page--gray page--login">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <UserNavigation />
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="page__main page__main--login">
         <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <LoginForm />
-          </section>
+          <LoginForm />
           <section className="locations locations--login locations--current">
             <div className="locations__item">
               <a className="locations__item-link" href="/#">
