4852e70ee8dc3c9d4ad2c4c35eb6de04b4f48785
Nikolay <nikolya_usov@bk.ru>
2021-07-22 14:58:24 +0300

    fix: error

--- project/src/components/loader/loader.test.jsx
+++ project/src/components/loader/loader.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Loader from './Loader';
+import Loader from './loader';
 
 
 describe('Test Map component', () => {
